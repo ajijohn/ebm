7f273c3e10e96e3d404149716f2a92b6a9509221
Aji John <aji.john@gmail.com>
2016-09-09 04:31:53 -0700

    static path fix

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
 app.use('/users', users);
 app.use('/exec', exec);
