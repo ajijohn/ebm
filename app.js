2377917230f2190e094332fae99c7d3985a7e736
Aji John <aji.john@gmail.com>
2016-09-20 02:53:26 -0700

    Added functionality to log the extraction request to MongoDB(First Pass)

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const compression = require('compression');
+var monk = require('monk');
 
+//TODO load the models
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var exec = require('./routes/exec');
 
 var app = express();
 
+// Backup Connection URL .
+var url = 'mongodb://localhost:27017/ebm';
+
+/**
+ * Connect to MongoDB.
+ */
+
+var db = monk(process.env.MONGODB_URI || process.env.MONGOLAB_URI || url);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(compression());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,10 +38,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/exec', exec);
+app.use('/extract', exec);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/exec.js
+++ routes/exec.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const
-    spawn = require( 'child_process' ).spawn;
+const spawn = require( 'child_process' ).spawn;
 
 
-/* GET home page. */
+/* Test */
+/*
 router.get('/', function(req, res, next) {
 
     var child = spawn('ls', [ '-lh', '/usr' ] );
@@ -21,5 +21,65 @@ router.get('/', function(req, res, next) {
 
     res.render('new', { title: 'Express' });
 });
+*/
+
+
+/* Write to the requests file */
+router.post('/', function(req, res, next) {
+
+    var db = req.db;
+    var requests = db.get('requests');
+
+    /*
+
+     misc:
+     email:aji.john@gmail.com
+     status:EMAILED
+     aggregationmetric:
+     lats:Array[3]
+     timelogged:
+     longs:Array[3]
+     variable:Array[1]
+     interval:
+     text:Request for extract
+     enddate:19810102
+     outputformat:csv
+     startdate:19810102
+
+     */
+
+
+    var variable = req.body.variable;
+    var lat = req.body.lat;
+    var lng = req.body.lng;
+    var stdate = req.body.startdate;
+    var eddate = req.body.enddate;
+    var file = req.body.file;
+    var email = req.body.email;
+    var dateperformed = req.body.dateperformed;
+    var new_request = {  misc:"",
+                         email:email,
+                         status:"OPEN",
+                         aggregationmetric:"",
+                         lats:[lat],
+                         timelogged:dateperformed,
+                          longs:[lng],
+                         variable:[variable],
+                         interval:"",
+                          text:"",
+                          enddate:eddate,
+                          outputformat:file,
+                          startdate:stdate};
+
+    //Using the {w:1} option ensure you get the error back if the document fails to insert correctly.
+    requests.insert(new_request, {w:1}, function(err, result) {});
+
+    res.json(201, {
+        success: 'Request logged'
+    });
+
+
+});
+
 
 module.exports = router;
