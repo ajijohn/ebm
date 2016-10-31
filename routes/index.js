b4f161d482668c9cab97d92f136e78f6d78e205a
Aji John <aji.john@gmail.com>
2016-10-31 01:53:39 -0700

    support google authentication

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',ensureAuthenticated,function(req, res, next) {
   res.render('index', { title: 'Energy Balance Modeling' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {res.render('login', { title: 'Energy Balance Modeling' });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 module.exports = router;
