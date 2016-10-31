1e578b94062622590f3e60a0dad30f7799078d39
Aji John <aji.john@gmail.com>
2016-10-31 14:47:12 -0700

    revamped the site structure

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/',ensureAuthenticated,function(req, res, next) {
+/* GET microclim page. */
+router.get('/',function(req, res, next) {
+  res.render('home', { title: 'Energy Balance Modeling' });
+});
+
+
+router.get('/signup',function(req, res, next) {
+  res.render('signup', { title: 'Energy Balance Modeling' });
+});
+
+/* GET microclim page. */
+router.get('/microclim',ensureAuthenticated,function(req, res, next) {
   res.render('index', { title: 'Energy Balance Modeling' });
 });
 
