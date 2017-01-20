81ddcc2343278e99ae2bfb0bb152d407cf3659fe
Aji John <aji.john@gmail.com>
2017-01-19 16:10:18 -0800

    Added functionality to update and get credentials

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const jwt = require('jsonwebtoken');
+const hat = require('hat');
+
 
 var router = express.Router();
 var corpsec = process.env.API_SEC || "default";
@@ -12,26 +14,154 @@ router.get('/', function(req, res, next) {
 /* GET apikey. */
 router.get('/account/apikey',ensureAuthenticated,function(req, res, next) {
 
-    var apikey= jwt.sign({
+    /*var apikey= jwt.sign({
         data: req.user.id
-    }, corpsec, { expiresIn: '365d' });
+    }, corpsec, { expiresIn: '365d' }); */
 
-    res.status(200).json({
-        apikey: apikey
+    var apikey=  hat();
+
+    //Update the API Key associated with the user
+
+    var db = req.db;
+    var users = db.get('users');
+
+    users.findOne({'user.email':req.user.email}).then(function(user){
+
+
+        if (user == null){
+            // rare case , user not updated, send blank
+            // TODO, fix the feedback
+            res.status(200).json({
+                apikey: ''
+            });
+        }
+        else
+        {
+            res.status(200).json({
+                apikey: user.apikey
+            });
+        }
+
+
     });
+
+
 });
 
 /* GET api secrets. */
 router.get('/account/apisecret',ensureAuthenticated,function(req, res, next) {
 
-    var apisec= jwt.sign({
+    /*var apisec= jwt.sign({
         data: req.user.id
-    }, corpsec, { expiresIn: '365d' });
+    }, corpsec, { expiresIn: '365d' }); */
 
-    res.status(200).json({
-        apisec: apisec
-    });
+    var apisec=  hat();
+    var db = req.db;
+    var users = db.get('users');
+
+    users.findOne({'user.email':req.user.email}).then(function(user){
+
+
+        if (user == null){
+            // rare case , user not updated, send blank
+            // TODO, fix the feedback
+            res.status(200).json({
+                apisec: ''
+            });
+        }
+        else
+        {
+            res.status(200).json({
+                apisec: user.apisecret
+            });
+        }
+
+
+        });
+
+
 });
+
+
+
+/* POST apikey - fetch new api key and update. */
+router.post('/account/apikey',ensureAuthenticated,function(req, res, next) {
+
+    /*var apikey= jwt.sign({
+     data: req.user.id
+     }, corpsec, { expiresIn: '365d' }); */
+
+    var apikey=  hat();
+
+    //Update the API Key associated with the user
+
+    var db = req.db;
+    var users = db.get('users');
+
+    users.findAndModify({ query: {'user.email':req.user.email},
+        update: { $set: {apikey: apikey }}, new:true}).then(function(user){
+
+
+        if (user == null){
+            // rare case , user not updated, send blank
+            // TODO, fix the feedback
+            res.status(200).json({
+                apikey: ''
+            });
+        }
+        else
+        {
+            res.status(200).json({
+                apikey: apikey
+            });
+        }
+
+
+    });
+
+
+});
+
+/* POST api secret - fetch api secret and update. */
+router.post('/account/apisecret',ensureAuthenticated,function(req, res, next) {
+
+    /*var apisec= jwt.sign({
+     data: req.user.id
+     }, corpsec, { expiresIn: '365d' }); */
+
+    var apisec=  hat();
+
+    var db = req.db;
+    var users = db.get('users');
+
+    users.findAndModify({ query: {'user.email':req.user.email},
+        update: { $set: {apisecret: apisec }}, new:true}).then(function(user){
+
+
+        if (user == null){
+            // rare case , user not updated, send blank
+            // TODO, fix the feedback
+            res.status(200).json({
+                apisec: ''
+            });
+        }
+        else
+        {
+            res.status(200).json({
+                apisec: apisec
+            });
+        }
+
+
+    });
+
+
+});
+
+
+
+
+
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
