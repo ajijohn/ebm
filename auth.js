2b748b5ddcf0b963b67d0fea9432aa74ad6d7292
Aji John <aji.john@gmail.com>
2021-04-13 12:08:51 -0700

    Delete auth.js

--- auth.js
+++
@@ -1,12 +0,0 @@
-/**
- * Created by ajijohn on 4/11/21.
- */
-/**  * Created by ajijohn on 10/31/16.  */ var ids = {
-    google: {
-        "clientID": "441814571052-0a21rbk89viktd41p3qteigriiqhbour.apps.googleusercontent.com",
-        "clientSecret": "mm1RjIrCGlYVO26qo6LJ9Oa8",
-        "callbackURL": "http://localhost:3000/auth/google/callback",
-        "realm": "http://localhost:3000"
-    }
-};
-module.exports = ids;
