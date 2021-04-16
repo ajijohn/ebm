4120d761fffa7cb61f49ec8cf8c5d8a0541705be
Sapti Sunil <sapti.s@innoneur.com>
2021-04-16 20:07:09 +0530

    UI issues with source WRF updated

--- auth.js
+++ auth.js
@@ -3,24 +3,24 @@
  */
 var ids= {
     linkedin : {
-        "api_key" : "",
-        "api_secret" : "",
+        "api_key" : "api_key",
+        "api_secret" : "api_secret",
         "callback_url" : "http://localhost:3000/auth/linkedin/callback",
         "realm":"http://localhost:3000"
     }
     ,
     twitter : {
-        "api_key" : "",
-        "api_secret" : "",
+        "api_key" : "api_key",
+        "api_secret" : "api_secret",
         "callback_url" : "http://localhost:3000/auth/twitter/callback",
         "realm":"http://localhost:3000"
     }
     ,
     google : {
-        "clientID" : "",
-        "clientSecret" : "",
-        "callbackURL" : "http://localhost:3002/auth/google/callback",
-        "realm" : "http://localhost:3002"
+        "clientID" : "client_ID",
+        "clientSecret" : "client_secret",
+        "callbackURL" : "http://localhost:3000/auth/google/callback",
+        "realm" : "http://localhost:3000"
     }
 };
 
